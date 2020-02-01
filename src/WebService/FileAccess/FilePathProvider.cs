06384ce58906a90439cf65e0b8673e0bcf21d171
Krzysztof_Tomkow <krzysztof.tomkow@gmail.com>
2020-02-01 22:54:43 +0100

    Created file provider implementation

---
+++ src/WebService/FileAccess/FilePathProvider.cs
@@ -0,0 +1,25 @@
+﻿using FileAccess;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebService.FileAccess
+{
+    public class FilePathProvider : IPathProvider
+    {
+        private readonly IWebHostEnvironment env;
+
+        public FilePathProvider(IWebHostEnvironment env)
+        {
+            this.env = env;
+        }
+
+        public string GetFilePath(string filename)
+        {
+            string path = $"{filename}.pdf";
+            return env.WebRootFileProvider.GetFileInfo(path)?.PhysicalPath;
+        }
+    }
+}
