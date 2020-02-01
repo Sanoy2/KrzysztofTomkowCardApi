242fe998ae174acfb02bc5125ceadbf9f3c5419c
Krzysztof_Tomkow <krzysztof.tomkow@gmail.com>
2020-02-01 23:06:01 +0100

    Removed Windsor because of no support for .net core 3

--- src/WebService/Startup.cs
+++ src/WebService/Startup.cs
@@ -10,7 +10,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using WebService.IoC;
 
 namespace WebService
 {
@@ -24,11 +23,9 @@ namespace WebService
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public IServiceProvider ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            var container = new ServiceResolver(services).GetServiceProvider();
-            return container;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
