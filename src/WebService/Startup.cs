7597a1bb2b346a807deec0ade4d15dbf0bef1529
Krzysztof_Tomkow <krzysztof.tomkow@gmail.com>
2020-02-01 11:23:47 +0100

    Run http on 5000, https on 5001, dont use https redirection

--- src/WebService/Startup.cs
+++ src/WebService/Startup.cs
@@ -36,7 +36,7 @@ namespace WebService
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
