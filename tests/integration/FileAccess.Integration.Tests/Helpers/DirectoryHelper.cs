e139eee82032f1153c9e4a86e3b9817fb947f733
sanoy2 <krzysztof.tomkow@gmail.com>
2020-03-01 20:03:54 +0100

    Created helper for integration file access tests

---
+++ tests/integration/FileAccess.Integration.Tests/Helpers/DirectoryHelper.cs
@@ -0,0 +1,44 @@
+using System;
+using System.IO;
+
+namespace FileAccess.Integration.Tests.Helpers
+{
+    internal sealed class DirectoryHelper
+    {
+        private readonly string directoryName;
+        private readonly string fullPath;
+
+        internal string DirectoryPath => this.GetPath();
+
+        public DirectoryHelper(string directoryName)
+        {
+            this.directoryName = directoryName;
+            this.fullPath = this.GetPath();
+        }
+
+        public void Dispose()
+        {
+            this.RemoveDirectory();
+        }
+
+        private void CreateDirectory()
+        {
+            Directory.CreateDirectory(this.fullPath);
+        }
+
+        private void RemoveDirectory()
+        {
+            Directory.Delete(this.fullPath, true);
+        }
+
+        private string GetPath()
+        {
+            return $"{this.GetCurrentDirectory()}/{this.directoryName}";
+        }
+
+        private string GetCurrentDirectory()
+        {
+            return Directory.GetCurrentDirectory();
+        }
+    }
+}
