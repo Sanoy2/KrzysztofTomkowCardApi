b436cd7156697aa18699a0f19b0543f8c019ea5f
sanoy2 <krzysztof.tomkow@gmail.com>
2020-03-10 21:50:31 +0100

    Test classes must be public. A bracket } was placed id wrong place

--- tests/unit/Common.Unit.Tests/EntityTests.cs
+++ tests/unit/Common.Unit.Tests/EntityTests.cs
@@ -8,45 +8,45 @@ namespace Common.Unit.Tests
     {
         private class SomeClass : Entity
         {
-            [Theory]
-            [InlineData(1)]
-            [InlineData(112)]
-            [InlineData(812646)]
-            [InlineData(int.MaxValue)]
-            [InlineData(long.MaxValue)]
-            public void Ctor_WhenId_Positive_ShouldNotThrowException(long id)
-            {
-                SomeClass someObject = new SomeClass(id);
-            }
-
-            public void Ctor_WhenId_0_ShouldThrowArgumentException()
-            {
-                long id = 0;
-
-                Action act = () =>
-                {
-                    SomeClass someObject = new SomeClass(id);
-                };
-
-                act.Should().ThrowExactly<ArgumentException>();
-            }
-
-            public void Ctor_WhenId_Negative_ShouldThrowArgumentException()
-            {
-                long id = -12;
-
-                Action act = () =>
-                {
-                    SomeClass someObject = new SomeClass(id);
-                };
-
-                act.Should().ThrowExactly<ArgumentException>();
-            }
-
             public SomeClass(long id) : base(id)
             {
 
             }
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(112)]
+        [InlineData(812646)]
+        [InlineData(int.MaxValue)]
+        [InlineData(long.MaxValue)]
+        public void Ctor_WhenId_Positive_ShouldNotThrowException(long id)
+        {
+            SomeClass someObject = new SomeClass(id);
+        }
+
+        public void Ctor_WhenId_0_ShouldThrowArgumentException()
+        {
+            long id = 0;
+
+            Action act = () =>
+            {
+                SomeClass someObject = new SomeClass(id);
+            };
+
+            act.Should().ThrowExactly<ArgumentException>();
+        }
+
+        public void Ctor_WhenId_Negative_ShouldThrowArgumentException()
+        {
+            long id = -12;
+
+            Action act = () =>
+            {
+                SomeClass someObject = new SomeClass(id);
+            };
+
+            act.Should().ThrowExactly<ArgumentException>();
         }
     }
 }
